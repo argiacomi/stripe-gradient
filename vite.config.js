b3c69f71270575f6caee515c368fdbc9616cee1a
Drew Giacomi <andrew.giacomi@gmail.com>
2023-03-15 14:43:20 -0400

    Updated vite.config for build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
-    disabled: false
+    disabled: true
   },
   plugins: [
     glsl({
